9f3a55eeb68a1d197468bead6f7658d921084c57
kkmike999 <kkmike999@163.com>
2026-03-06 02:58:19 +0800

    B站上传脚本

---
+++ bilibili/upload_bilibili.sh
@@ -0,0 +1,13 @@
+biliup upload \
+  "D:\YouTube\bilibili\temp\【ASMR】眼精疲労  疲れ目マッサージ (ホットストーン・銀コロ・リファを使って) 00_00_00-00_01_03.mp4" \
+  --cover "D:\YouTube\bilibili\temp\【ASMR】眼精疲労  疲れ目マッサージ (ホットストーン・銀コロ・リファを使って).jpg" \
+  --title "【ASMR】眼精疲労  疲れ目マッサージ (ホットストーン・銀コロ・リファを使って)" \
+  --copyright 2 \
+  --source "https://www.youtube.com/@TOKYOASMRMASSAGE" \
+  --tid 21 \
+  --extra-fields '{"human_type2": 1026}' \
+  --tag "生活,日常,娱乐,按摩,减压,MASSAGE,日本,放松,美女,SPA" \
+  --desc $'留意置顶评论区！\n留意置顶评论区！\n留意置顶评论区！\n麻烦转发、点赞、收藏，一件三连！[OK]' \
+  --dolby 1 \
+  --hires 1 \
+  --dtime $(date -d "2026-03-10 20:30" +%s)
